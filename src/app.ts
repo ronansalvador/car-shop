867fe6e99345c27c602b5e3f61f31d785e4daf91
Ronan Salvador <ronansalvador@yahoo.com.br>
2023-01-13 15:34:55 -0300

    feat(route): criando rota cars no arquivo app

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,8 @@
 import express from 'express';
+import carsRoute from './routes/CarsRoute';
 
 const app = express();
+app.use(express.json());
+app.use('/cars', carsRoute);
 
 export default app;
-
-// iniciando
