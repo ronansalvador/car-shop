611d3b92730490ed5176847bea7190fe3cde73a5
Ronan Salvador <ronansalvador@yahoo.com.br>
2023-01-16 08:37:11 -0300

    feat(test): atualizando teste referente a rota motorcycles na camada service

--- tests/Mock/MotorcycleMock.ts
+++ tests/Mock/MotorcycleMock.ts
@@ -15,4 +15,27 @@ const createdMotorcycle = {
   id: '63c40ae5c3608a04fca388ae',
 };
 
-export { createMotorcycle, createdMotorcycle };
+const motorcycleMock = [
+  {
+    id: '63c40ae5c3608a04fca388ae',
+    model: 'Honda Cb 600f Hornet',
+    year: 2005,
+    color: 'Yellow',
+    status: true,
+    buyValue: 30.000,
+    category: 'Street',
+    engineCapacity: 600,
+  },
+  {
+    id: '634852326b35b59438fbea31',
+    model: 'Honda Cbr 1000rr',
+    year: 2011,
+    color: 'Orange',
+    status: true,
+    buyValue: 59.900,
+    category: 'Street',
+    engineCapacity: 1000,
+  },
+];
+
+export { createMotorcycle, createdMotorcycle, motorcycleMock };
--- tests/unit/Services/MotorcycleService.test.ts
+++ tests/unit/Services/MotorcycleService.test.ts
@@ -2,9 +2,14 @@ import { expect } from 'chai';
 import { Model } from 'mongoose';
 import sinon from 'sinon';
 import MotorcycleService from '../../../src/Services/MotorcycleService';
-import { createMotorcycle, createdMotorcycle } from '../../Mock/MotorcycleMock';
+import { createMotorcycle, createdMotorcycle, motorcycleMock } from '../../Mock/MotorcycleMock';
 
 describe('Testes MotorcycleService', function () {
+  const motorcycleService = new MotorcycleService();
+
+  afterEach(function () {
+    sinon.restore();
+  });
   it('Verifica se é possível criar uma nova moto', async function () {
     sinon.stub(Model, 'create').resolves(createdMotorcycle);
 
@@ -12,5 +17,56 @@ describe('Testes MotorcycleService', function () {
     const created = await service.create(createMotorcycle);
 
     expect(created).to.be.deep.equal(createdMotorcycle);
+  });
+  it('Verifica erro se uma nova moto não for criada', async function () {
+    try {
+      await motorcycleService.create({
+        model: '',
+        year: 2022,
+        color: 'Black',
+        buyValue: 15.900,
+        category: 'Street',
+        engineCapacity: 600,
+      });
+    } catch (err) {
+      expect((err as Error).message).to.be
+        .equal('Motorcycle validation failed: model: Path `model` is required.');
+    }
+  });
+
+  it('Verifica se é possível buscar todas as motos', async function () {
+    sinon.stub(Model, 'find').resolves(motorcycleMock);
+
+    const fetchCars = await motorcycleService.findAll();
+
+    expect(fetchCars).to.deep.equal(motorcycleMock);
+  });
+
+  it('Verifica se é possível buscar uma moto por id', async function () {
+    sinon.stub(Model, 'findOne').resolves(createdMotorcycle);
+
+    const findById = await motorcycleService.findById('63c40ae5c3608a04fca388ae');
+
+    expect(findById).to.deep.equal(createdMotorcycle);
+  });
+
+  it('Ocorre um erro ao procurar o id de uma moto que não existe', async function () {
+    sinon.stub(Model, 'findOne').resolves(null);
+
+    try {
+      await motorcycleService.findById('634852326b35b59438fbea2f');
+    } catch (err) {
+      expect((err as Error).message).to.be.equal('Motorcycle not found');
+    }
+  });
+
+  it('Verifica erro ao tentar buscar por um id com formato inválido', async function () {
+    sinon.stub(Model, 'findOne').resolves(null);
+
+    try {
+      await motorcycleService.findById('1234');
+    } catch (err) {
+      expect((err as Error).message).to.be.equal('Invalid mongo id');
+    }
   });
 });
